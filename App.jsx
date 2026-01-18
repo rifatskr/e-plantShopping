f6670bb2ef0e2b4f110084f3e7490632e0a35df3
Rifat Sarkar <101231997+rifatskr@users.noreply.github.com>
2026-01-19 03:37:50 +0600

    App.jsx

--- App.jsx
+++ App.jsx
@@ -1,1 +1,36 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import ProductList from "./ProductList";
+import CartItem from "./CartItem";
+import AboutUs from "./AboutUs";
+import "./App.css";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div className="landing-page">
+              <h1>Paradise Nursery</h1>
+              <p>
+                Discover a wide variety of indoor plants to refresh your home
+                and workspace.
+              </p>
+              <Link to="/products">
+                <button className="get-started-btn">Get Started</button>
+              </Link>
+            </div>
+          }
+        />
+        <Route path="/products" element={<ProductList />} />
+        <Route path="/cart" element={<CartItem />} />
+        <Route path="/about" element={<AboutUs />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
 
